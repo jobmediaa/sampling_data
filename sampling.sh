8a68eadf2779a3215c887474df3eef2dd8770506
Job Media <105165148+jobmediaa@users.noreply.github.com>
2022-05-09 01:25:43 +0700

    Update sampling.sh

--- sampling.sh
+++ sampling.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#!/usr/bin/bash
+#download
+wget -P ./data https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
 
-curl -o  https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
+#convert sheet to csv
+in2csv ./data/weather_data.xlsx --sheet "weather_2014" > ./data/weather_2014.csv
+in2csv ./data/weather_data.xlsx --sheet "weather_2015" > ./data/weather_2015.csv
+
+#append data
+csvstack ./data/weather_2014.csv ./data/weather_2015.csv > ./data/weather.csv
+
+#remove data
+rm ./data/weather_data.xlsx
+
+#sampling data with rate 0.3
+cat ./data/weather.csv | sample -r 0.3 > ./data/sample_weather.csv
